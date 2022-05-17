31e785b1b8f1eab2c513e637fffbf9cb19148b13
Emerson <emerson.day@gmail.com>
2022-05-17 16:10:33 -0500

    added json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,108 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Search = ({filter, handleFilterChange}) => {
+  return (
+    <>
+      find <input value={filter} onChange={handleFilterChange}/>
+    </>
+  )
+}
+
+const Contacts = ({filteredContacts}) => {
+  return(
+    <>
+      <h2>Numbers</h2>
+      {filteredContacts.map(person => {
+        return <ContactInfo key={person.name}  person={person}/>
+      })}
+    </>
+  )
+}
+
+const ContactInfo = ({person}) => {
+  return(
+    <>
+      <p key={person.id}>{person.name} - {person.number}</p>
+    </>
+  )
+}
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+const addName = (e) =>{
+  e.preventDefault()
+  console.log('button clicked', e.target)
+  const nameObj = {
+    name: newName, number: newNumber, id: persons.length + 1
+  }
+  const isFound = persons.some(element => {
+    if (element.name === newName){
+      return true
+    }
+    return false;
+  })
+    if(isFound){
+      alert(`${newName} is already in the phone book`)
+      setNewName('')
+    }else{
+      setPersons(persons.concat(nameObj))
+  setNewName('')
+  setNewNumber('')
+    }
+}
+
+const handleNameChange = (e) => {
+  setNewName(e.target.value)
+}
+
+const handleNumberChange = (e) => {
+  setNewNumber(e.target.value)
+}
+const handleFilterChange = (e) => {
+  setFilter(e.target.value)
+  console.log(filter)
+ 
+}
+
+const filteredContacts = persons.filter((person) => {
+  return person.name.toLowerCase().includes(filter) 
+})
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <h4>-------------------------------------</h4>
+      <Search onChange={handleFilterChange} filter={filter}/>
+      <h4>-------------------------------------</h4>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addName}>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+     <Contacts filteredContacts={filteredContacts}/>
+      
     </div>
   )
 }
